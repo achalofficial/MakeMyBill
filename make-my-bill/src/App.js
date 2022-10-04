c8daa90caa932cbbf991c3cba5d5142867e32481
Achal <osndl.achal@aicofindia.com>
2022-10-04 17:33:17 +0530

    Food Component Initiated

---
+++ make-my-bill/src/App.js
@@ -0,0 +1,12 @@
+import FoodItem from "./components/FoodItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World !!!</h1>
+      <FoodItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ make-my-bill/src/components/FoodItem.js
@@ -0,0 +1,11 @@
+import "./FoodItem.css";
+
+function FoodItem() {
+  return (
+    <div>
+      <h1>This is Food Item </h1>
+    </div>
+  );
+}
+
+export default FoodItem;
